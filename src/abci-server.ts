41f97ed4e172d7769e01c388c9826f1b77ee8559
Mark Jackson <fumecow@gmail.com>
2018-11-01 09:57:21 -0600

    add success return code to state transition return struct

--- src/abci-server.ts
+++ src/abci-server.ts
@@ -17,7 +17,7 @@ export default function createABCIServer(stateMachine, initialState): any {
       try {
         let tx = decodeTx(request.tx)
         try {
-          return stateMachine.transition({ type: 'transaction', data: tx })
+          return Object.assign({ code: 0 }, stateMachine.transition({ type: 'transaction', data: tx }))
         } catch (e) {
           return { code: 1, log: e.toString() }
         }
